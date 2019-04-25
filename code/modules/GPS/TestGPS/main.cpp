df4dcdc2cfdf8156d144d677da054b9b22e9ce5d
firefly <389049674@qq.com>
2019-04-25 12:41:31 +0000

    First time to compile on board

--- code/modules/GPS/TestGPS/main.cpp
+++ code/modules/GPS/TestGPS/main.cpp
@@ -32,6 +32,7 @@ void init()
     // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
     //GPS.sendCommand(PMTK_SET_BAUD_57600);
     GPS.begin(9600);
+
     // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
     GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
     // uncomment this line to turn on only the "minimum recommended" data
