9b53b2763db062dbfcbb615c2b857de8a02b1093
fanzhuzhi <389049674@qq.com>
2019-05-07 14:54:47 +0200

    One backup

--- code/modules/mpu9250/TestDataCalibration/main.cpp
+++ code/modules/mpu9250/TestDataCalibration/main.cpp
@@ -7,9 +7,9 @@ int main()
     cout << "Hello world!" << endl;
 
     std::default_random_engine generator;
-    std::normal_distribution<float> distributionX(1.0,0.2);
-    std::normal_distribution<float> distributionY(1.5,0.25);
-    std::normal_distribution<float> distributionZ(2.0,0.25);
+    std::normal_distribution<float> distributionX(40.0,400);
+    std::normal_distribution<float> distributionY(15.5,150);
+    std::normal_distribution<float> distributionZ(26.0,200);
 
     DataCalibration DataRamdom3Axi;
 
