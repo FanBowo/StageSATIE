731fade9757cddaa6eab692fdaca26ca7f212057
firefly <389049674@qq.com>
2019-05-16 15:24:42 +0000

    Succeced migration

---
+++ code/modules/BNO 055/src/util/LinuxClk.cpp
@@ -0,0 +1,18 @@
+#include "LinuxClk.h"
+
+int Linux_get_clock_ms(int32_t *count)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	*count = (tv.tv_sec)*1000+(tv.tv_usec / 1000);
+	return 0;
+}
+
+int Linux_delay_ms(unsigned long num_ms)
+{
+	//mdelay(num_ms);
+	for(unsigned long i=0; i<num_ms;i++){
+		usleep(1000);
+	}
+	return 0;
+}
