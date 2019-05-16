c6e22e7ef73fc9b28fdb8749a18d35795da00549
fanzhuzhi <389049674@qq.com>
2019-05-16 10:40:36 +0200

    Add module BNO 055

---
+++ code/modules/BNO 055/utility/LinuxClk.h
@@ -0,0 +1,17 @@
+#ifndef _LINUXCLK_H_
+#define _LINUXCLK_H_
+#include <unistd.h>
+#include <sys/time.h>
+#include <stdint.h>
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+    int Linux_get_clock_ms(int32_t*count);
+
+    int Linux_delay_ms(unsigned long num_ms);
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // _LINUXCLK_H_
