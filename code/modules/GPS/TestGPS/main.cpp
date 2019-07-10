31fcc358128b8bc3122519130d30bb07cff70200
firefly <389049674@qq.com>
2019-07-10 09:42:06 +0000

    Increase baud rate of gps

--- code/modules/GPS/TestGPS/main.cpp
+++ code/modules/GPS/TestGPS/main.cpp
@@ -30,9 +30,10 @@ void init()
 
     std::cout<<"Adafruit GPS library basic test!"<<std::endl;
     // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-    //GPS.sendCommand(PMTK_SET_BAUD_57600);
-    GPS.begin(9600);
 
+    GPS.begin(9600);
+    GPS.sendCommand(PMTK_SET_BAUD_57600);
+    GPS.begin(57600);
     // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
     GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
     // uncomment this line to turn on only the "minimum recommended" data
