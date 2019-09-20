b6e281bab94550baf64c16108a6552bcdedf6fa8
fanzhuzhi <389049674@qq.com>
2019-09-20 14:14:14 +0200

    Rename threads

--- code/modules/Assemble/Assemble/main.cpp
+++ code/modules/Assemble/Assemble/main.cpp
@@ -64,150 +64,155 @@ int main() // run over and over again
 
     /*
         Interrupt: GPS serial port int to send signal of updating base timestamp
+		Interrupt: Timer interrupt of updating device timestamp timer
         Interrupt: IMU Timer interrupt to send signal of updating IMU timestamp
                     and of reading raw IMU data
         Interrupt: Cameras Timer interrupt to send signal to take photo
         Interrupt: Receiving a frame of photo Interrupt to save photo into RAM and
                     to read IMU direction data
-        Interrupt: Timer interrupt of updating device timestamp timer
+        
 
         Task:
-        Task1:Update base time stamp
-        Task2:Read IMU raw data and save into fifo
-        Task3:Save raw IMU data to csv file
-        Task4-7:Save photos and save IMU direction data to csv file
-        Task8:Save photos and save IMU direction data to fifo
-        Task9: Update time stamp
-
-        Schedule policy: RR
-        Task1:MaxPriorityRR
-        Task2:MaxPriorityRR-3
-        Task3:MaxPriorityRR-25
-        Task4-7:MaxPriorityRR-20
-        Task8:MaxPriorityRR-2
-        Task9:MaxPriorityRR-1
+		
+        Task1:THREAD_RENOUVELER_BASE_HORODATAGE:Update base time stamp
+		MaxPriorityRR-2
+		
+		Task2: THREAD_RENOUVELER_ HORODATAGE:Update time stamp
+		MaxPriorityRR-6
+		
+		Task3:THREAD_RENOUVELER_ GPS_FIFO:Update GPS Data fifo
+		MaxPriorityRR-6
+		
+		Task4:THREAD_LIRE_IMU_BRU:Read IMU raw data and save into fifo
+		MaxPriorityRR-10
+		
+		Task5:THREAD_SAUVEGARDER_BRUT_IMU:Save raw IMU data to csv file
+		MaxPriorityRR-30
+		
+		Task6-9:THREAD_SAUVEGARDER_BRUT_IMU:Save photos and save IMU direction data to csv file
+		MaxPriorityRR-30
+		
+		Task10:THREAD_LIRE_IMAGE:Get photos and save IMU direction data to fifo
+		MaxPriorityRR-12
+		
+		Task11:THREAD_SAUVEGARDER_INFO_GPS:Save raw GPS data to csv file
+        MaxPriorityRR-30
+   
     */
 
     std::cout<<"Initialization finished"<<std::endl;
 
-    pthread_t ThreadUpdateTimeStampBase;
-    struct sched_param ThreadUpdateTimeStampBasePara;
-    memset(&ThreadUpdateTimeStampBasePara,0,sizeof(sched_param));
-    ThreadUpdateTimeStampBasePara.__sched_priority=sched_get_priority_max(SCHED_RR)-2;
-    pthread_attr_t ThreadUpdateTimeStampBaseParaAttr;
-    pthread_attr_init(&ThreadUpdateTimeStampBaseParaAttr);
-    pthread_attr_setinheritsched(&ThreadUpdateTimeStampBaseParaAttr,PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&ThreadUpdateTimeStampBaseParaAttr,SCHED_RR);
-    pthread_attr_setschedparam(&ThreadUpdateTimeStampBaseParaAttr,&ThreadUpdateTimeStampBasePara);
-    pthread_create(&ThreadUpdateTimeStampBase,&ThreadUpdateTimeStampBaseParaAttr,&UpdateTimeStampBaseFunc,NULL);
+    pthread_t THREAD_RENOUVELER_BASE_HORODATAGE;
+    struct sched_param THREAD_RENOUVELER_BASE_HORODATAGEPara;
+    memset(&THREAD_RENOUVELER_BASE_HORODATAGEPara,0,sizeof(sched_param));
+    THREAD_RENOUVELER_BASE_HORODATAGEPara.__sched_priority=sched_get_priority_max(SCHED_RR)-2;
+    pthread_attr_t THREAD_RENOUVELER_BASE_HORODATAGEParaAttr;
+    pthread_attr_init(&THREAD_RENOUVELER_BASE_HORODATAGEParaAttr);
+    pthread_attr_setinheritsched(&THREAD_RENOUVELER_BASE_HORODATAGEParaAttr,PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&THREAD_RENOUVELER_BASE_HORODATAGEParaAttr,SCHED_RR);
+    pthread_attr_setschedparam(&THREAD_RENOUVELER_BASE_HORODATAGEParaAttr,&THREAD_RENOUVELER_BASE_HORODATAGEPara);
+    pthread_create(&THREAD_RENOUVELER_BASE_HORODATAGE,&THREAD_RENOUVELER_BASE_HORODATAGEParaAttr,&UpdateTimeStampBaseFunc,NULL);
 
-    pthread_t ThreadDevice_UpdateTimeStamp;
-    struct sched_param ThreadDevice_UpdateTimeStampPara;
-    memset(&ThreadDevice_UpdateTimeStampPara,0,sizeof(sched_param));
-    ThreadDevice_UpdateTimeStampPara.__sched_priority=sched_get_priority_max(SCHED_RR)-6;
-    pthread_attr_t ThreadDevice_UpdateTimeStampParaAttr;
-    pthread_attr_init(&ThreadDevice_UpdateTimeStampParaAttr);
-    pthread_attr_setinheritsched(&ThreadDevice_UpdateTimeStampParaAttr,PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&ThreadDevice_UpdateTimeStampParaAttr,SCHED_RR);
-    pthread_attr_setschedparam(&ThreadDevice_UpdateTimeStampParaAttr,&ThreadDevice_UpdateTimeStampPara);
-    pthread_create(&ThreadDevice_UpdateTimeStamp,&ThreadDevice_UpdateTimeStampParaAttr,&UpdateDeviceTimeStampFunc,NULL);
+    pthread_t THREAD_RENOUVELER_ HORODATAGE;
+    struct sched_param THREAD_RENOUVELER_ HORODATAGEPara;
+    memset(&THREAD_RENOUVELER_ HORODATAGEPara,0,sizeof(sched_param));
+    THREAD_RENOUVELER_ HORODATAGEPara.__sched_priority=sched_get_priority_max(SCHED_RR)-6;
+    pthread_attr_t THREAD_RENOUVELER_ HORODATAGEParaAttr;
+    pthread_attr_init(&THREAD_RENOUVELER_ HORODATAGEParaAttr);
+    pthread_attr_setinheritsched(&THREAD_RENOUVELER_ HORODATAGEParaAttr,PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&THREAD_RENOUVELER_ HORODATAGEParaAttr,SCHED_RR);
+    pthread_attr_setschedparam(&THREAD_RENOUVELER_ HORODATAGEParaAttr,&THREAD_RENOUVELER_ HORODATAGEPara);
+    pthread_create(&THREAD_RENOUVELER_ HORODATAGE,&THREAD_RENOUVELER_ HORODATAGEParaAttr,&UpdateDeviceTimeStampFunc,NULL);
 
-    pthread_t ThreadGPS_UpdateFIFO;
-    struct sched_param ThreadGPS_UpdateFIFOPara;
-    memset(&ThreadGPS_UpdateFIFOPara,0,sizeof(sched_param));
-    ThreadGPS_UpdateFIFOPara.__sched_priority=sched_get_priority_max(SCHED_RR)-3;
-    pthread_attr_t ThreadGPS_UpdateFIFOParaAttr;
-    pthread_attr_init(&ThreadGPS_UpdateFIFOParaAttr);
-    pthread_attr_setinheritsched(&ThreadGPS_UpdateFIFOParaAttr,PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&ThreadGPS_UpdateFIFOParaAttr,SCHED_RR);
-    pthread_attr_setschedparam(&ThreadGPS_UpdateFIFOParaAttr,&ThreadGPS_UpdateFIFOPara);
-    pthread_create(&ThreadGPS_UpdateFIFO,&ThreadGPS_UpdateFIFOParaAttr,&GPS_UpdateFIFOFunc,NULL);
+    pthread_t THREAD_RENOUVELER_ GPS_FIFO;
+    struct sched_param THREAD_RENOUVELER_ GPS_FIFOPara;
+    memset(&THREAD_RENOUVELER_ GPS_FIFOPara,0,sizeof(sched_param));
+    THREAD_RENOUVELER_ GPS_FIFOPara.__sched_priority=sched_get_priority_max(SCHED_RR)-3;
+    pthread_attr_t THREAD_RENOUVELER_ GPS_FIFOParaAttr;
+    pthread_attr_init(&THREAD_RENOUVELER_ GPS_FIFOParaAttr);
+    pthread_attr_setinheritsched(&THREAD_RENOUVELER_ GPS_FIFOParaAttr,PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&THREAD_RENOUVELER_ GPS_FIFOParaAttr,SCHED_RR);
+    pthread_attr_setschedparam(&THREAD_RENOUVELER_ GPS_FIFOParaAttr,&THREAD_RENOUVELER_ GPS_FIFOPara);
+    pthread_create(&THREAD_RENOUVELER_ GPS_FIFO,&THREAD_RENOUVELER_ GPS_FIFOParaAttr,&GPS_UpdateFIFOFunc,NULL);
 
-    pthread_t ThreadIMU_UpdateRawData;
-    struct sched_param ThreadIMU_UpdateRawDataPara;
-    memset(&ThreadIMU_UpdateRawDataPara,0,sizeof(sched_param));
-    ThreadIMU_UpdateRawDataPara.__sched_priority=sched_get_priority_max(SCHED_RR)-10;
-    pthread_attr_t ThreadIMU_UpdateRawDataParaAttr;
-    pthread_attr_init(&ThreadIMU_UpdateRawDataParaAttr);
-    pthread_attr_setinheritsched(&ThreadIMU_UpdateRawDataParaAttr,PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&ThreadIMU_UpdateRawDataParaAttr,SCHED_RR);
-    pthread_attr_setschedparam(&ThreadIMU_UpdateRawDataParaAttr,&ThreadIMU_UpdateRawDataPara);
-    pthread_create(&ThreadIMU_UpdateRawData,&ThreadIMU_UpdateRawDataParaAttr,&IMU_UpdateRawDataFunc,NULL);
+    pthread_t THREAD_LIRE_IMU_BRU;
+    struct sched_param THREAD_LIRE_IMU_BRUPara;
+    memset(&THREAD_LIRE_IMU_BRUPara,0,sizeof(sched_param));
+    THREAD_LIRE_IMU_BRUPara.__sched_priority=sched_get_priority_max(SCHED_RR)-10;
+    pthread_attr_t THREAD_LIRE_IMU_BRUParaAttr;
+    pthread_attr_init(&THREAD_LIRE_IMU_BRUParaAttr);
+    pthread_attr_setinheritsched(&THREAD_LIRE_IMU_BRUParaAttr,PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&THREAD_LIRE_IMU_BRUParaAttr,SCHED_RR);
+    pthread_attr_setschedparam(&THREAD_LIRE_IMU_BRUParaAttr,&THREAD_LIRE_IMU_BRUPara);
+    pthread_create(&THREAD_LIRE_IMU_BRU,&THREAD_LIRE_IMU_BRUParaAttr,&IMU_UpdateRawDataFunc,NULL);
 
-//    while(1){
-//        pthread_mutex_lock(& bIMU_Data_StableMutex );
-//            if(AssembleDevice.bIMU_Data_Stable){
-//                break;
-//            }
-//        pthread_mutex_unlock(& bIMU_Data_StableMutex );
-//    }
 
-    pthread_t ThreadSaveIMU_RawData;
-    struct sched_param ThreadSaveIMU_RawDataPara;
-    memset(&ThreadSaveIMU_RawDataPara,0,sizeof(sched_param));
-    ThreadSaveIMU_RawDataPara.__sched_priority=sched_get_priority_max(SCHED_RR)-30;
-    pthread_attr_t ThreadSaveIMU_RawDataParaAttr;
-    pthread_attr_init(&ThreadSaveIMU_RawDataParaAttr);
-    pthread_attr_setinheritsched(&ThreadSaveIMU_RawDataParaAttr,PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&ThreadSaveIMU_RawDataParaAttr,SCHED_RR);
-    pthread_attr_setschedparam(&ThreadSaveIMU_RawDataParaAttr,&ThreadSaveIMU_RawDataPara);
-    pthread_create(&ThreadSaveIMU_RawData,&ThreadSaveIMU_RawDataParaAttr,&SaveIMU_RawDataFunc,NULL);
+    pthread_t THREAD_SAUVEGARDER_BRUT_IMU;
+    struct sched_param THREAD_SAUVEGARDER_BRUT_IMUPara;
+    memset(&THREAD_SAUVEGARDER_BRUT_IMUPara,0,sizeof(sched_param));
+    THREAD_SAUVEGARDER_BRUT_IMUPara.__sched_priority=sched_get_priority_max(SCHED_RR)-30;
+    pthread_attr_t THREAD_SAUVEGARDER_BRUT_IMUParaAttr;
+    pthread_attr_init(&THREAD_SAUVEGARDER_BRUT_IMUParaAttr);
+    pthread_attr_setinheritsched(&THREAD_SAUVEGARDER_BRUT_IMUParaAttr,PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&THREAD_SAUVEGARDER_BRUT_IMUParaAttr,SCHED_RR);
+    pthread_attr_setschedparam(&THREAD_SAUVEGARDER_BRUT_IMUParaAttr,&THREAD_SAUVEGARDER_BRUT_IMUPara);
+    pthread_create(&THREAD_SAUVEGARDER_BRUT_IMU,&THREAD_SAUVEGARDER_BRUT_IMUParaAttr,&SaveIMU_RawDataFunc,NULL);
 
-    pthread_t ThreadSaveCamera_IMU_Data;
-    struct sched_param ThreadSaveCamera_IMU_DataPara;
-    memset(&ThreadSaveCamera_IMU_DataPara,0,sizeof(sched_param));
-    ThreadSaveCamera_IMU_DataPara.__sched_priority=sched_get_priority_max(SCHED_RR)-30;
-    pthread_attr_t ThreadSaveCamera_IMU_DataParaAttr;
-    pthread_attr_init(&ThreadSaveCamera_IMU_DataParaAttr);
-    pthread_attr_setstacksize(&ThreadSaveCamera_IMU_DataParaAttr,33554432);
-    pthread_attr_setinheritsched(&ThreadSaveCamera_IMU_DataParaAttr,PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&ThreadSaveCamera_IMU_DataParaAttr,SCHED_RR);
-    pthread_attr_setschedparam(&ThreadSaveCamera_IMU_DataParaAttr,&ThreadSaveCamera_IMU_DataPara);
-    pthread_create(&ThreadSaveCamera_IMU_Data,&ThreadSaveCamera_IMU_DataParaAttr,&SaveCamera_IMU_DataFunc,NULL);
+    pthread_t THREAD_SAUVEGARDER_CAMERA_IMU;
+    struct sched_param THREAD_SAUVEGARDER_CAMERA_IMUPara;
+    memset(&THREAD_SAUVEGARDER_CAMERA_IMUPara,0,sizeof(sched_param));
+    THREAD_SAUVEGARDER_CAMERA_IMUPara.__sched_priority=sched_get_priority_max(SCHED_RR)-30;
+    pthread_attr_t THREAD_SAUVEGARDER_CAMERA_IMUParaAttr;
+    pthread_attr_init(&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr);
+    pthread_attr_setstacksize(&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr,33554432);
+    pthread_attr_setinheritsched(&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr,PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr,SCHED_RR);
+    pthread_attr_setschedparam(&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr,&THREAD_SAUVEGARDER_CAMERA_IMUPara);
+    pthread_create(&THREAD_SAUVEGARDER_CAMERA_IMU,&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr,&SaveCamera_IMU_DataFunc,NULL);
 
-    pthread_t ThreadSaveCamera_IMU_Data1;
-    pthread_create(&ThreadSaveCamera_IMU_Data1,&ThreadSaveCamera_IMU_DataParaAttr,&SaveCamera_IMU_DataFunc,NULL);
+    pthread_t THREAD_SAUVEGARDER_CAMERA_IMU1;
+    pthread_create(&THREAD_SAUVEGARDER_CAMERA_IMU1,&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr,&SaveCamera_IMU_DataFunc,NULL);
 
-    pthread_t ThreadSaveCamera_IMU_Data2;
-    pthread_create(&ThreadSaveCamera_IMU_Data2,&ThreadSaveCamera_IMU_DataParaAttr,&SaveCamera_IMU_DataFunc,NULL);
+    pthread_t THREAD_SAUVEGARDER_CAMERA_IMU2;
+    pthread_create(&THREAD_SAUVEGARDER_CAMERA_IMU2,&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr,&SaveCamera_IMU_DataFunc,NULL);
 
-//    pthread_t ThreadSaveCamera_IMU_Data3;
-//    pthread_create(&ThreadSaveCamera_IMU_Data3,&ThreadSaveCamera_IMU_DataParaAttr,&SaveCamera_IMU_DataFunc,NULL);
+//    pthread_t THREAD_SAUVEGARDER_CAMERA_IMU3;
+//    pthread_create(&THREAD_SAUVEGARDER_CAMERA_IMU3,&THREAD_SAUVEGARDER_CAMERA_IMUParaAttr,&SaveCamera_IMU_DataFunc,NULL);
 
-    pthread_t ThreadSaveCamera_IMU_DataToFifo;
-    struct sched_param ThreadSaveCamera_IMU_DataToFifoPara;
-    memset(&ThreadSaveCamera_IMU_DataToFifoPara,0,sizeof(sched_param));
-    ThreadSaveCamera_IMU_DataToFifoPara.__sched_priority=sched_get_priority_max(SCHED_RR)-12;
-    pthread_attr_t ThreadSaveCamera_IMU_DataToFifoParaAttr;
-    pthread_attr_init(&ThreadSaveCamera_IMU_DataToFifoParaAttr);
-    pthread_attr_setstacksize(&ThreadSaveCamera_IMU_DataToFifoParaAttr,33554432);
-    pthread_attr_setinheritsched(&ThreadSaveCamera_IMU_DataToFifoParaAttr,PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&ThreadSaveCamera_IMU_DataToFifoParaAttr,SCHED_RR);
-    pthread_attr_setschedparam(&ThreadSaveCamera_IMU_DataToFifoParaAttr,&ThreadSaveCamera_IMU_DataToFifoPara);
-    pthread_create(&ThreadSaveCamera_IMU_DataToFifo,&ThreadSaveCamera_IMU_DataToFifoParaAttr,&SaveCamera_IMU_DataToFifoFunc,NULL);
+    pthread_t THREAD_LIRE_IMAGE;
+    struct sched_param THREAD_LIRE_IMAGEPara;
+    memset(&THREAD_LIRE_IMAGEPara,0,sizeof(sched_param));
+    THREAD_LIRE_IMAGEPara.__sched_priority=sched_get_priority_max(SCHED_RR)-12;
+    pthread_attr_t THREAD_LIRE_IMAGEParaAttr;
+    pthread_attr_init(&THREAD_LIRE_IMAGEParaAttr);
+    pthread_attr_setstacksize(&THREAD_LIRE_IMAGEParaAttr,33554432);
+    pthread_attr_setinheritsched(&THREAD_LIRE_IMAGEParaAttr,PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&THREAD_LIRE_IMAGEParaAttr,SCHED_RR);
+    pthread_attr_setschedparam(&THREAD_LIRE_IMAGEParaAttr,&THREAD_LIRE_IMAGEPara);
+    pthread_create(&THREAD_LIRE_IMAGE,&THREAD_LIRE_IMAGEParaAttr,&SaveCamera_IMU_DataToFifoFunc,NULL);
 
-    pthread_t ThreadSaveGPS_Data;
-    struct sched_param ThreadSaveGPS_DataPara;
-    memset(&ThreadSaveGPS_DataPara,0,sizeof(sched_param));
-    ThreadSaveGPS_DataPara.__sched_priority=sched_get_priority_max(SCHED_RR)-30;
-    pthread_attr_t ThreadSaveGPS_DataParaAttr;
-    pthread_attr_init(&ThreadSaveGPS_DataParaAttr);
-    pthread_attr_setinheritsched(&ThreadSaveGPS_DataParaAttr,PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&ThreadSaveGPS_DataParaAttr,SCHED_RR);
-    pthread_attr_setschedparam(&ThreadSaveGPS_DataParaAttr,&ThreadSaveGPS_DataPara);
-    pthread_create(&ThreadSaveGPS_Data,&ThreadSaveGPS_DataParaAttr,&SaveGPS_DataFunc,NULL);
+    pthread_t THREAD_SAUVEGARDER_INFO_GPS;
+    struct sched_param THREAD_SAUVEGARDER_INFO_GPSPara;
+    memset(&THREAD_SAUVEGARDER_INFO_GPSPara,0,sizeof(sched_param));
+    THREAD_SAUVEGARDER_INFO_GPSPara.__sched_priority=sched_get_priority_max(SCHED_RR)-30;
+    pthread_attr_t THREAD_SAUVEGARDER_INFO_GPSParaAttr;
+    pthread_attr_init(&THREAD_SAUVEGARDER_INFO_GPSParaAttr);
+    pthread_attr_setinheritsched(&THREAD_SAUVEGARDER_INFO_GPSParaAttr,PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&THREAD_SAUVEGARDER_INFO_GPSParaAttr,SCHED_RR);
+    pthread_attr_setschedparam(&THREAD_SAUVEGARDER_INFO_GPSParaAttr,&THREAD_SAUVEGARDER_INFO_GPSPara);
+    pthread_create(&THREAD_SAUVEGARDER_INFO_GPS,&THREAD_SAUVEGARDER_INFO_GPSParaAttr,&SaveGPS_DataFunc,NULL);
 
-    pthread_join(ThreadUpdateTimeStampBase,NULL);
-    pthread_join(ThreadDevice_UpdateTimeStamp,NULL);
-    pthread_join(ThreadGPS_UpdateFIFO,NULL);
-    pthread_join(ThreadIMU_UpdateRawData,NULL);
-    pthread_join(ThreadSaveIMU_RawData,NULL);
-    pthread_join(ThreadSaveCamera_IMU_Data,NULL);
-    pthread_join(ThreadSaveCamera_IMU_Data1,NULL);
-    pthread_join(ThreadSaveCamera_IMU_Data2,NULL);
-//    pthread_join(ThreadSaveCamera_IMU_Data3,NULL);
-    pthread_join(ThreadSaveCamera_IMU_DataToFifo,NULL);
-    pthread_join(ThreadSaveGPS_Data,NULL);
+    pthread_join(THREAD_RENOUVELER_BASE_HORODATAGE,NULL);
+    pthread_join(THREAD_RENOUVELER_ HORODATAGE,NULL);
+    pthread_join(THREAD_RENOUVELER_ GPS_FIFO,NULL);
+    pthread_join(THREAD_LIRE_IMU_BRU,NULL);
+    pthread_join(THREAD_SAUVEGARDER_BRUT_IMU,NULL);
+    pthread_join(THREAD_SAUVEGARDER_CAMERA_IMU,NULL);
+    pthread_join(THREAD_SAUVEGARDER_CAMERA_IMU1,NULL);
+    pthread_join(THREAD_SAUVEGARDER_CAMERA_IMU2,NULL);
+//    pthread_join(THREAD_SAUVEGARDER_CAMERA_IMU3,NULL);
+    pthread_join(THREAD_LIRE_IMAGE,NULL);
+    pthread_join(THREAD_SAUVEGARDER_INFO_GPS,NULL);
 
 
     std::cout<<"End of programme"<<std::endl;
